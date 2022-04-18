2ab1559b2a608dcbdd24732c8f385fda2453d67a
Ejan Shrestha <60786649+ijanggmu@users.noreply.github.com>
2022-04-18 16:49:45 +0545

    Orange and Apple Problem

---
+++ HackerRank/OrangeApple.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank
+{
+    public class OrangeApple
+    {
+        public static void OrangeApples(int s ,int t,int m,int n,List<int> apple,List<int> mango)
+        {
+            int countM = 0;
+            int countA = 0;
+            foreach (var i in apple)
+            {
+                int sumA = i + m;
+                if (sumA >= s && sumA <= t)
+                {
+                    countA++;
+                }
+
+            }
+            foreach (var i in mango)
+            {
+                int sumM = i + n;
+                if (sumM >= s && sumM <= t)
+                {
+                    countM++;
+                }
+
+            }
+            Console.WriteLine(countA + " " + countM);
+            //apple.ForEach(n =>n.Equals(n+a));
+            //mango.ForEach(n => Console.WriteLine(n+b));          
+            //Console.WriteLine(apple.Sum() +" "+ mango.Sum());
+            Console.WriteLine($"{countA} a d {countM}");
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -49,18 +49,112 @@ namespace HackerRank
             //MinMax
             //List<int> arr = new List<int>() { 1,2,3,4,5};
             //MinMax.miniMaxSum(arr);
-            //BirthdayCake
-            Console.WriteLine("Please enter no integer: ");
-            int candlesCount = Convert.ToInt32(Console.ReadLine());
-            var candles = new List<int>();
-            for (int i = 0; i < candlesCount; i++)
-            {
-                Console.WriteLine($"Enter {i+1} no:");
-                candles.Add(Convert.ToInt32(Console.ReadLine()));
-            }
-            int result = BirthdayCake.birthdayCakeCandles(candles);
+            ////BirthdayCake
+            //Console.WriteLine("Please enter no integer: ");
+            //int candlesCount = Convert.ToInt32(Console.ReadLine());
+            //var candles = new List<int>();
+            //for (int i = 0; i < candlesCount; i++)
+            //{
+            //    Console.WriteLine($"Enter {i+1} no:");
+            //    candles.Add(Convert.ToInt32(Console.ReadLine()));
+            //}
+            //int result = BirthdayCake.birthdayCakeCandles(candles);
 
-            Console.WriteLine(result);
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            //    //TimeConversion
+            //    DateTime d = DateTime.Parse(s);
+            //    return d.ToString("HH:mm:ss");
+            //}
+            //var reult = new List<int>();
+            //var nmul = new List<int>();
+            //var grade = new List<int>() { 84, 90,45,38,39 };
+            //for (int i = 0; i < grade.Count; i++)
+            //{
+            //    int fac = grade[i] % 5;
+            //    if (fac == 0 || grade[i]<38)
+            //    {
+            //        reult.Add(grade[i]);
+            //    }
+            //    else
+            //    {
+            //        int mofive = ((grade[i] / 5)+1)*5;
+            //        int diff = 5 - fac;
+            //        if (diff < 3)
+            //        {
+            //            reult.Add(mofive);
+            //        };
+
+
+
+            //    };
+
+            //};
+            //foreach(var i in reult){
+            //    Console.WriteLine(i);
+            //};
+        
+            //var apple = new List<int>()
+            //{
+            //    -2,2,1
+            //};
+            //var mango = new List<int>()
+            //{
+            //    5,-6
+            //};
+            //int countM = 0;
+            //int countA = 0;
+            //foreach (var i in apple)
+            //{
+            //    int sumA = i + a;
+            //    if (sumA>=s && sumA <= t)
+            //    {
+            //        countA++;
+            //    }
+
+            //}
+            //foreach (var i in mango) {
+            //    int sumM = i + m;
+            //    if (sumM >= s && sumM <= t)
+            //    {
+            //        countM++;
+            //    }
+
+            //    }
+            //Console.WriteLine(countA +" "+countM);
+            ////apple.ForEach(n =>n.Equals(n+a));
+            ////mango.ForEach(n => Console.WriteLine(n+b));          
+            ////Console.WriteLine(apple.Sum() +" "+ mango.Sum());
+            ////Console.WriteLine($"{countA} a d {countM}");
+            ///
+            //APplemango problem
+            //Console.WriteLine("Enter the no of Apple");
+            //int m= Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the no of Mango");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the distance of start House");
+            //int s = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the distance of end House");
+            //int t = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the distance of Apple ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the distance of Mango ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+           
+            //var apple = new List<int>();
+            //var mango = new List<int>();
+            //for(int i=0;i<m;i++)
+            //{
+            //    Console.WriteLine($"apple {i+1}");
+            //    apple.Add(Convert.ToInt32(Console.ReadLine()));
+            //};
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine($"mango {i + 1}");
+            //    mango.Add(Convert.ToInt32(Console.ReadLine()));
+            //}
+            //OrangeApple.OrangeApples(s, t, a, b, apple, mango);
             Console.ReadLine();
         }
     }
